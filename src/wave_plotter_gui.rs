f3d2782578bf33be086247a35b32610cb0b34bce
Majorx234 <majorx234@googlemail.com>
2023-01-28 23:12:01 +0100

    Add wave plotting in GUI

--- src/wave_plotter_gui.rs
+++ src/wave_plotter_gui.rs
@@ -5,7 +5,7 @@ use std::sync::Mutex;
 
 use egui::plot::{
     Arrows, Bar, BarChart, CoordinatesFormatter, Corner, GridInput, GridMark, HLine, Legend, Line,
-    LineStyle, MarkerShape, Plot, PlotImage, Points, Polygon, Text, VLine,
+    LineStyle, MarkerShape, Plot, PlotImage, Points, Polygon, Text, VLine, Values,
 };
 
 pub struct WavePlotterGui {
@@ -24,6 +24,12 @@ impl eframe::App for WavePlotterGui {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("WavePlotterGui");
+            let plot = Plot::new("audio-signal");
+            plot.show(ui, |plot_ui| {
+                plot_ui.points(Points::new(Values::from_ys_f32(
+                    &self.audiodata.lock().unwrap().get_values(),
+                )));
+            });
         });
     }
 }
